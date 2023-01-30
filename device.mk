0f226e564639f8e4b35c810e98e44e060146245e
Anay Wadhera <anay1018@gmail.com>
2023-01-30 19:10:41 +0000

    alioth: add NFC overlay secure nfc support
    Change-Id: I45df56069a05d9fba81cc8c705ac7703fe431a13
    Signed-off-by: Anay Wadhera <anay1018@gmail.com>

--- device.mk
+++ device.mk
@@ -32,7 +32,8 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/AliothNFCOverlay
 
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
